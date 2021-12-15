cac00450d81c08955617de37f4837072a45175a0
lorib <lori.barto@live.it>
2021-12-15 16:22:46 +0100

    start plumbing

--- helloindigo/src/HelloIndigo.scala
+++ helloindigo/src/HelloIndigo.scala
@@ -6,24 +6,19 @@ object HelloIndigo extends IndigoSandbox[Unit, Model] {
 
   val magnification = 3
 
-  val config: GameConfig =
+  val config: indigo.GameConfig =
     GameConfig.default.withMagnification(magnification)
 
   val animations: Set[Animation] =
     Set()
 
+  val assetName = AssetName("dots")
   val easy      = AssetName("easy")
-  val medium    = AssetName("medium")
-  val hard      = AssetName("hard")
-  val nightmare = AssetName("nightmare")
 
-  val assets: Set[AssetType] =
-    Set(
-      AssetType.Image(easy, AssetPath("assets/90.png")),
-      AssetType.Image(medium, AssetPath("assets/60.png")),
-      AssetType.Image(hard, AssetPath("assets/47.png")),
-      AssetType.Image(nightmare, AssetPath("assets/10.png"))
-    )
+  val assets: Set[indigo.AssetType] = Set(
+    AssetType.Image(assetName, AssetPath("assets/dots.png")),
+    AssetType.Image(easy, AssetPath("assets/90.png"))
+  )
 
   val fonts: Set[FontInfo] =
     Set()
@@ -38,41 +33,13 @@ object HelloIndigo extends IndigoSandbox[Unit, Model] {
     Outcome(Startup.Success(()))
 
   def initialModel(startupData: Unit): Outcome[Model] =
-    Outcome(
-      Model.initial(
-        config.viewport.giveDimensions(magnification).center
-      )
-    )
+    Outcome(Model.initial())
 
   def updateModel(
       context: FrameContext[Unit],
       model: Model
-  ): GlobalEvent => Outcome[Model] = {
-    case MouseEvent.Click(pt) =>
-      val adjustedPosition = pt - model.center
-
-      Outcome(
-        model
-          .addDot(
-            Dot(
-              Point.distanceBetween(model.center, pt).toInt,
-              Radians(
-                Math.atan2(
-                  adjustedPosition.x.toDouble,
-                  adjustedPosition.y.toDouble
-                )
-              )
-            )
-          )
-          .reverse()
-      )
-
-    case FrameTick =>
-      Outcome(model.update(context.delta))
-
-    case _ =>
-      Outcome(model)
-  }
+  ): GlobalEvent => Outcome[Model] =
+    _ => Outcome(model)
 
   def present(
       context: FrameContext[Unit],
@@ -80,55 +47,18 @@ object HelloIndigo extends IndigoSandbox[Unit, Model] {
   ): Outcome[SceneUpdateFragment] =
     Outcome(
       SceneUpdateFragment(
-        Graphic(Rectangle(0, 0, 32, 32), 1, Material.Bitmap(easy))
+        Graphic(Rectangle(0, 0, 96, 96), 1, Material.Bitmap(easy).stretch)
+          .withRef(48, 48)
+          .moveTo(config.viewport.giveDimensions(magnification).center)
       )
     )
-
-  // def drawDots(
-  //     center: Point,
-  //     dots: List[Dot]
-  // ): List[Graphic[_]] =
-  //   dots.map { dot =>
-  //     val position = Point(
-  //       (Math.sin(dot.angle.toDouble) * dot.orbitDistance + center.x).toInt,
-  //       (Math.cos(dot.angle.toDouble) * dot.orbitDistance + center.y).toInt
-  //     )
-
-  //     Graphic(Rectangle(0, 0, 32, 32), 1, Material.Bitmap(assetName))
-  //       .withCrop(Rectangle(16, 16, 16, 16))
-  //       .withRef(8, 8)
-  //       .moveTo(position)
-  //   }
-
 }
 
-enum DotsDirection:
-  case Clockwise, CounterClockwise
-
-case class Model(center: Point, dots: List[Dot], direction: DotsDirection) {
-  def addDot(dot: Dot): Model =
-    this.copy(dots = dot :: dots)
-
-  def reverse(): Model =
-    this.copy(direction =
-      if (direction == DotsDirection.Clockwise) DotsDirection.CounterClockwise else DotsDirection.Clockwise
-    )
-
+final case class Model(level: Int, angle: Radians) {
   def update(timeDelta: Seconds): Model =
-    this.direction match {
-      case DotsDirection.Clockwise        => this.copy(dots = dots.map(_.update(timeDelta)))
-      case DotsDirection.CounterClockwise => this.copy(dots = dots.map(_.reverseUpdate(timeDelta)))
-    }
-
+    this.copy(angle = angle + Radians.fromSeconds(timeDelta))
 }
 
 object Model {
-  def initial(center: Point): Model = Model(center, Nil, DotsDirection.Clockwise)
-}
-
-case class Dot(orbitDistance: Int, angle: Radians) {
-  def update(timeDelta: Seconds): Dot =
-    this.copy(angle = angle + Radians.fromSeconds(timeDelta))
-  def reverseUpdate(timeDelta: Seconds): Dot =
-    this.copy(angle = angle - Radians.fromSeconds(timeDelta))
+  def initial(): Model = Model(0, Radians(0))
 }
